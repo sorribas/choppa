7e25e0d8292ec44213e402479cca5ade9d64b8d0
Eduardo Sorribas <eduardo@sorribas.org>
2014-08-16 18:05:16 +0200

    Add random support.

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 var through = require('through2');
 
 var choppa = function(chunkSize) {
-  chunkSize = chunkSize || 1;
+  chunkSize = chunkSize === undefined ? 1 : chunkSize;
   var prev = new Buffer(0);
 
   var transform = function(chunk, enc, cb) {
     chunk = Buffer.concat([prev, chunk]);
     var self = this;
-    while (chunk.length >= chunkSize) {
-      self.push(chunk.slice(0, chunkSize));
-      chunk = chunk.slice(chunkSize);
+    if (chunkSize > 0) {
+      while (chunk.length >= chunkSize) {
+        self.push(chunk.slice(0, chunkSize));
+        chunk = chunk.slice(chunkSize);
+      }
+      prev = chunk;
+    } else {
+      while (chunk.length) {
+        var size = Math.floor(Math.random() * chunk.length) + 1;
+        self.push(chunk.slice(0, size));
+        chunk = chunk.slice(size);
+      }
     }
 
-    prev = chunk;
     cb();
   };
 
